770ef23bd80c668b495608e8b646a524c239b69a
Olga Iamshchikova <oljabotova@gmail.com>
2023-09-26 20:54:06 +0200

    Initial commit

---
+++ src/test/java/io/yamyamiya/telegram/bot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yamyamiya.telegram.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
